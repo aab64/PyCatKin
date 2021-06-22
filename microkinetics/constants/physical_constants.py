2f35858c93e432050275feffa0470f2cfa9ac3c5
aab64 <aab64@cam.ac.uk>
2021-06-22 11:52:10 +0200

    Temporarily use butadiene constants.

--- microkinetics/constants/physical_constants.py
+++ microkinetics/constants/physical_constants.py
@@ -2,15 +2,26 @@
 
 """
 
-kB = 1.38064900e-23
-h = 6.62607015e-34
+# kB = 1.38064900e-23
+# h = 6.62607015e-34
+# JtoeV = 6.242e18
+# eVtokJ = 96.485
+# eVtokcal = 23.06
+# kcaltoJ = 4184
+# amutokg = 1.66053907e-27
+# amuA2tokgm2 = 1.66053907e-47
+# R = 8.31446262
+NA = 6.02214076e23
+bartoPa = 1.0e5
+atmtoPa = 1.01325e5
+
+# Butadiene paper
+kB = 1.380662e-23
+h = 6.626176e-34
 JtoeV = 6.242e18
 eVtokJ = 96.485
 eVtokcal = 23.06
 kcaltoJ = 4184
-amutokg = 1.66053907e-27
+amutokg = 1.66053886e-27
 amuA2tokgm2 = 1.66053907e-47
 R = 8.31446262
-NA = 6.02214076e23
-bartoPa = 1.0e5
-atmtoPa = 1.01325e5
