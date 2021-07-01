704775953359b3b8a1b0d4270e973a1eb4249c77
aab64 <47423435+aab64@users.noreply.github.com>
2021-07-01 17:39:44 +0200

    remove imageconverter extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ author = 'Astrid Boje'
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.imgconverter']
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
