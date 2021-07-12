a5a814121870148636c60c0460dc6ebdaabe9760
aab64 <47423435+aab64@users.noreply.github.com>
2021-07-12 12:50:41 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,8 +69,8 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-html_logo = '_static/avatar/avatar.png'
+html_static_path = ['_static']
+html_logo = 'avatar.png'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
