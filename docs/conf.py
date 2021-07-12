a8f96ef4f609c84eb81b221a8fb06285a162016c
aab64 <47423435+aab64@users.noreply.github.com>
2021-07-12 12:00:12 +0200

    try again

--- docs/conf.py
+++ docs/conf.py
@@ -59,12 +59,6 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_logo = '_static/avatar/avatar.png'
-html_theme = 'sphinx_rtd_theme'
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -75,7 +69,13 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_logo = 'avatar/avatar.png'
+html_theme = 'sphinx_rtd_theme'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
