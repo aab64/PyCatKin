d5d09ff801c3ab6a7f96776a615037faf3531789
aab64 <47423435+aab64@users.noreply.github.com>
2021-07-12 12:05:45 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -70,11 +70,11 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = ['_static/avatar/avatar.png']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_logo = 'avatar/avatar.png'
 html_theme = 'sphinx_rtd_theme'
 
 # Custom sidebar templates, must be a dictionary that maps document names
