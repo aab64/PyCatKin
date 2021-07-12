3d001cf13ee417840d30d5015b49427ec6ede8f6
aab64 <47423435+aab64@users.noreply.github.com>
2021-07-12 12:21:31 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -64,8 +64,9 @@ pygments_style = 'sphinx'
 # documentation.
 #
 html_theme_options = {
-		     'prev_next_buttons_location': 'both'
-                     'logo': 'avatar/avatar.png',}
+		     'prev_next_buttons_location': 'both',
+                     'logo': 'avatar/avatar.png'
+}
              
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
