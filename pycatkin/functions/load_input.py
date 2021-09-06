0a7cd7fe2155c46cf89bafd6a099589243dfdc63
aab64 <bojeastrid@gmail.com>
2021-09-06 14:13:22 +0200

    Adjusted input reading for reaction derived reactions.

--- pycatkin/functions/load_input.py
+++ pycatkin/functions/load_input.py
@@ -70,6 +70,7 @@ def read_from_input_file(input_path='input.json'):
         raise RuntimeError('Input file contains no system details.')
 
     reactions = None
+    reaction_derived_reactions = []
     if 'reactions' in pck_system.keys():
         print('Reading reactions:')
         reactions = dict()
@@ -93,6 +94,20 @@ def read_from_input_file(input_path='input.json'):
             if reactions[r].TS is not None:
                 reactions[r].TS = [sim_system.states[s] for s in reactions[r].TS]
 
+    if 'reaction derived reactions' in pck_system.keys():
+        if reactions is None:
+            print('Reading reactions:')
+            reactions = dict()
+        for r in pck_system['reaction derived reactions'].keys():
+            print('* %s' % r)
+            reactions[r] = ReactionDerivedReaction(name=r, **pck_system['reaction derived reactions'][r])
+            reactions[r].reactants = [sim_system.states[s] for s in reactions[r].reactants]
+            reactions[r].products = [sim_system.states[s] for s in reactions[r].products]
+            if reactions[r].TS is not None:
+                reactions[r].TS = [sim_system.states[s] for s in reactions[r].TS]
+            reactions[r].base_reaction = reactions[reactions[r].base_reaction]
+            reaction_derived_reactions.append(reactions[r].base_reaction.name)
+
     if reactions is not None:
         for r in reactions.keys():
             for s in reactions[r].reactants + reactions[r].products:
@@ -106,7 +121,8 @@ def read_from_input_file(input_path='input.json'):
                         for sr in s.scaling_reactions.keys():
                             if isinstance(s.scaling_reactions[sr]['reaction'], str):
                                 s.scaling_reactions[sr]['reaction'] = reactions[s.scaling_reactions[sr]['reaction']]
-            sim_system.add_reaction(reaction=reactions[r])
+            if r not in reaction_derived_reactions:
+                sim_system.add_reaction(reaction=reactions[r])
 
     if 'reactor' in pck_system.keys():
         print('Reading reactor:')
